b4c98a19f885b774354499b2180ea7b2232f898b
a14a19 <anas31197@gmail.com>
2023-12-28 09:21:38 +0530

    unauthorized

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -6,11 +6,11 @@ export const authAdminRole = (req, res, next) => {
         if (decode.role === "admin") {
             next();
         } else {
-            return res.status(404).send({ message: "Not authorized", error: undefined, data: undefined, status: false });
+            return res.status(404).send({ message: "Unauthorized", error: undefined, data: undefined, status: false });
         }
     } catch (e) {
         // console.log("auth role: ", e)
-        return res.status(404).send({ auth: "no auth", error: e })
+        return res.status(404).send({ auth: "Unauthorized", error: e })
     }
 }
 
@@ -19,10 +19,10 @@ export const authRole = (req, res, next) => {
         if (req.headers['authorization'].split(" ")[1]) {
             next();
         } else {
-            return res.status(404).send({ message: "Not authorized", error: undefined, data: undefined, status: false });
+            return res.status(404).send({ message: "Unauthorized", error: undefined, data: undefined, status: false });
         }
     } catch (e) {
         // console.log("auth role: ", e)
-        return res.status(404).send({ auth: "no auth", error: e })
+        return res.status(404).send({ auth: "Unauthorized", error: e })
     }
 }
